0e8b7ef61cb1b9ae4babd7a3f43422f93510a598
nkpedji <florencenkpedji9@gmail.com>
2023-08-18 20:48:02 +0200

    modif crawler

--- crawler.py
+++ crawler.py
@@ -1,7 +1,7 @@
 import requests
 
 API_KEY = "YOUR_OPENWEATHERMAP_API_KEY"
-BASE_URL = "http://api.openweathermap.org/"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 def get_weather(city_name):
     params = {
