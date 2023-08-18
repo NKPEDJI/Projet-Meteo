d7e1032b33d8eddf414c4960188b7067b7e03fe8
nkpedji <florencenkpedji9@gmail.com>
2023-08-18 22:42:40 +0200

    modif api key crawler

--- crawler.py
+++ crawler.py
@@ -1,15 +1,19 @@
 import requests
 
-API_KEY = "YOUR_OPENWEATHERMAP_API_KEY"
-BASE_URL = "https://openweathermap.org/"
+API_KEY = "cbedf9374cc81594a05d3ed33264416a"
+city_name = "Paris"
+BASE_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
+
 def get_weather(city_name):
     params = {
         "q": city_name,
         "appid": API_KEY,
-        "units": "metric"  # Pour obtenir les températures en Celsius
+        "units": "metric"  # To get temperatures in Celsius
     }
 
     response = requests.get(BASE_URL, params=params)
+    print("get response")
+    print(response.status_code)
 
     if response.status_code == 200:
         data = response.json()
@@ -19,12 +23,6 @@ def get_weather(city_name):
         humidity = data["main"]["humidity"]
         wind_speed = data["wind"]["speed"]
 
-        #print(f"Weather in {city_name}:")
-        #print(f"Description: {weather_description}")
-        #print(f"Temperature: {temperature}°C")
-        #print(f"Humidity: {humidity}%")
-        #print(f"Wind Speed: {wind_speed} m/s")
-
         weather_data = {
             "City": city_name,
             "Description": weather_description,
@@ -33,13 +31,11 @@ def get_weather(city_name):
             "Wind Speed": wind_speed
         }
         return weather_data
-
     else:
         print("Error fetching weather data")
 
 if __name__ == "__main__":
-    city_name = "Paris"  # Nom de la ville pour laquelle vous souhaitez obtenir les informations météorologiques
-    #get_weather(city_name)
+    city_name = "Paris"
     weather_info = get_weather(city_name)
 
     if weather_info:
