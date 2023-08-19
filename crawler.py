b42fd642603d47da5dccb0e588a94ef4cb2bf498
nkpedji <florencenkpedji9@gmail.com>
2023-08-19 18:10:43 +0200

    interrogation et extraction des infos pertinentes -- modif crawler.py

--- crawler.py
+++ crawler.py
@@ -1,48 +1,66 @@
 import requests
+import json
 
 API_KEY = "cbedf9374cc81594a05d3ed33264416a"
-city_name = "Paris"
-BASE_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
 
-def get_weather(city_name):
 
-    params = {
+# URL de l'API pour les informations météorologiques
+#API_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
 
-        "q": city_name,
-        "appid": API_KEY,
-        "units": "metric"  # To get temperatures in Celsius
 
-    }
-    response = requests.get(BASE_URL, params=params)
+# Chargement des données des villes depuis le fichier city.list.json
+with open("city.list.json", "r", encoding="utf-8") as json_file:
+    city_data = json.load(json_file)
 
-    if response.status_code == 200:
-        data = response.json()
+# Liste pour stocker les informations météorologiques des villes de France
+weather_data = []
 
-        weather_description = data["weather"][0]["description"]
-        temperature = data["main"]["temp"]
-        humidity = data["main"]["humidity"]
-        wind_speed = data["wind"]["speed"]
+# Boucle à travers les villes
+for city in city_data:
+    if city["country"] == "FR": 
+        #print("condition france validé") # Filtrer les villes de France
+        city_name = city["name"]
+        #print(city_name)
+        city_id = city["id"]
 
-        weather_data = {
-            "City": city_name,
-            "Description": weather_description,
-            "Temperature": temperature,
-            "Humidity": humidity,
-            "Wind Speed": wind_speed
+        API_URL = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}"
+
+        # Paramètres pour l'appel à l'API
+        params = {
+            "q": city_name,
+            "appid": API_KEY,
+            "units": "metric" 
         }
-        return weather_data
-    else:
-        print("Error fetching weather data")
 
- 
+        # Appel à l'API
+        response = requests.get(API_URL, params=params)
+        
+        if response.status_code == 200:
+            data = response.json()
+            #print("appel api reussi")
 
-if __name__ == "__main__":
-    city_name = "Paris"
-    weather_info = get_weather(city_name)
+            # Extraction des informations pertinentes
+            weather_info = {
+                "city_name": city_name,
+                "temperature": data["main"]["temp"],
+                "humidity": data["main"]["humidity"],
+                "wind_speed": data["wind"]["speed"],
+                "weather_description": data["weather"][0]["description"]               
+            }
+            print("infos pertinents reussi")
 
-    if weather_info:
-        print("Weather Information:")
-        for key, value in weather_info.items():
-            print(f"{key}: {value}")
+            # Ajout des informations à la liste
+            weather_data.append(weather_info)
+            #print(weather_data)
+
+# Affichage des informations météorologiques
+for info in weather_data:
+    print(f"City: {info['city_name']}")
+    print(f"Temperature: {info['temperature']} C")
+    print(f"Humidity: {info['humidity']}")
+    print(f"wind_speed: {info['wind_speed']}")
+    print(f"Weather Description: {info['weather_description']}")
+    print("-" * 30)
+    print("méteo")
 
  
